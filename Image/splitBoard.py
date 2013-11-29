4e69cd04c318639b1004babef1ba2915456ed2e9
Grzegorz Gajoch <gajoch@gmail.com>
2013-11-29 01:53:18 +0100

    Image module clear. Added new tests, now just add calibration.

--- Image/splitBoard.py
+++ Image/splitBoard.py
@@ -1,15 +1,22 @@
 import cv2
+import copy
+
 import numpy as np
+
 
 #noinspection PyTrailingSemicolon
 def searchForPawn(img):
+    """function returns:
+        0 - no figure
+        1 - white
+        -1 - black"""
     #global img
     #img2 = cv2.imread(r"C:\Users\rexina\Dropbox\AGH\MiTP\cz.jpg")
     img2 = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
     img2 = cv2.medianBlur(img2,5)
     #cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-    circles = cv2.HoughCircles(img2,cv2.cv.CV_HOUGH_GRADIENT,1,20,param1=43,param2=28,minRadius=0,maxRadius=0)
+    circles = cv2.HoughCircles(img2, cv2.cv.CV_HOUGH_GRADIENT, 1, 20, param1=29, param2=26, minRadius=15, maxRadius=0)
 
     if circles is None:
         print "NO_PAWN"
@@ -17,6 +24,8 @@ def searchForPawn(img):
         return 0
     print circles
     circles = np.uint16(np.around(circles))
+    #if len(circles) > 1:
+    #   return 0
     i = circles[0][0]
 
 
@@ -44,14 +53,16 @@ def searchForPawn(img):
         print "WHITE PAWN"
         cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
         cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+        return 1
     else:
         print "BLACK PAWN"
         cv2.circle(img,(i[0],i[1]),i[2],(0,0,255),2)
         cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
-    
+        return -1
 
 
-    #cv2.imshow('detected circles',img)
+
+        #cv2.imshow('detected circles',img)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
@@ -60,61 +71,72 @@ ix = iy = 0
 
 
 def mouseEvent(event, x, y, flags, param):
-    global ix, iy, drawing,img,points
+    global ix, iy, drawing, imgCopy, points, left
     if event == cv2.EVENT_LBUTTONDOWN:
-        print x,y
+        print [x, y]
         points.append([x,y])
-        cv2.circle(img,(x,y),10,(0,0,255),-1)
-        
-
-img = cv2.imread(r"C:\Users\rexina\Dropbox\AGH\MiTP\asm.jpg")
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',mouseEvent)
-
-rows,cols,ch = img.shape
-pts1 = np.float32([[70,152],[392,159],[68,461],[381,469]])
-pts2 = np.float32([[0,0],[600,0],[0,600],[600,600]])
-
-M = cv2.getPerspectiveTransform(pts1,pts2)
-
-dst = cv2.warpPerspective(img,M,(600,600))
+        cv2.circle(imgCopy, (x, y), 5, (0, 255, 0), -1)
 
 
-#points = [[66, 12], [176, 17], [288, 20], [401, 25], [512, 27], [626, 29], [735, 30], [844, 31], [948, 31], [62, 121], [180, 131], [288, 130], [402, 130], [510, 136], [623, 139], [731, 145], [837, 142], [948, 143], [62, 236], [174, 237], [290, 243], [399, 242], [507, 245], [617, 246], [720, 248], [843, 247], [940, 255], [60, 348], [169, 347], [283, 353], [397, 352], [504, 355], [610, 355], [723, 358], [833, 358], [941, 364], [68, 446], [169, 446], [284, 457], [395, 456], [502, 458], [615, 458], [723, 464], [834, 467], [943, 465], [58, 552], [166, 553], [281, 555], [390, 558], [500, 558], [603, 563], [725, 565], [828, 570], [937, 569], [60, 663], [165, 663], [275, 667], [386, 666], [499, 667], [606, 671], [722, 669], [829, 677], [937, 676], [52, 773], [160, 773], [275, 773], [391, 773], [494, 779], [606, 774], [715, 781], [831, 783], [931, 786], [50, 874], [162, 883], [274, 885], [386, 883], [492, 884], [605, 886], [721, 884], [825, 891], [927, 894]]
-points = []
-for i in range(1,602,75):
-    for j in range(1,602,75):
-        points.append([j, i])
-print points
+if __name__ == "__main__":
+    #os.system("wget http://192.168.137.124:8080/shot.jpg -O shot.jpg")
+    img = cv2.imread(r"BW.jpg")
+    imgCopy = copy.deepcopy(img)
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', mouseEvent)
 
-print len(points)
+    points = []
+    points = [[111, 148], [404, 140], [112, 443], [413, 443]]
+    while True:
+        cv2.imshow('image', imgCopy)
+        if len(points) == 4 or cv2.waitKey(1) & 0xFF == 27:
+            break
 
+    cv2.destroyAllWindows()
+    cv2.namedWindow('image')
 
-dic = {}
-for i in range(0,9):
-    for j in range(0,9):
-        dic[i,j] = points[9*i+j]
+    rows, cols, ch = img.shape
+    #pts1 = np.float32([[70, 152],[392, 159],[68, 461],[381, 469]])
+    pts1 = np.float32([points[i] for i in range(0, 4)])
+    pts2 = np.float32([[0, 0], [600, 0], [0, 600], [600, 600]])
 
-print dic[0,0],dic[1,1]
+    M = cv2.getPerspectiveTransform(pts1, pts2)
 
-for i in range(0,8):
-    for j in range(0,8):
-        x1 = dic[i,j][0]
-        y1 = dic[i,j][1]
-        x2 = dic[i+1,j+1][0]
-        y2 = dic[i+1,j+1][1]
-        #cv2.imshow('image',img[y1:y2,x1:x2])
-        searchForPawn(dst[y1:y2,x1:x2])
-        #cv2.waitKey(0)
+    dst = cv2.warpPerspective(img, M, (600, 600))
 
-for i in points:
-    cv2.circle (dst,tuple(i),3,(0,0,255),-1)
+    points = []
+    for i in range(1, 602, 75):
+        for j in range(1, 602, 75):
+            points.append([j, i])
+    print points
 
-while True:
-    cv2.imshow('image',dst)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+    print len(points)
 
-print points
-cv2.destroyAllWindows()
+    dic = {}
+    for i in range(0, 9):
+        for j in range(0, 9):
+            dic[i, j] = points[9 * i + j]
+
+    print dic[0, 0], dic[1, 1]
+
+    for i in range(0, 8):
+        for j in range(0, 8):
+            x1 = dic[i, j][0]
+            y1 = dic[i, j][1]
+            x2 = dic[i + 1, j + 1][0]
+            y2 = dic[i + 1, j + 1][1]
+            #cv2.imshow('image',img[y1:y2,x1:x2])
+            searchForPawn(dst[y1:y2, x1:x2])
+            #cv2.waitKey(0)
+
+    for i in points:
+        cv2.circle(dst, tuple(i), 3, (0, 0, 255), -1)
+
+    while True:
+        cv2.imshow('image', dst)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    print points
+    cv2.destroyAllWindows()
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Draughts
 ========
 
 Automatic draughts player
+
+TODO:
+- for each field add constants table, automatically filling during calibration.
